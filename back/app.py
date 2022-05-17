761f281b4e6b42d648e131d92d20e373dbfa1a63
dordiales92 <dordiales92@gmail.com>
2022-05-17 14:31:34 +0200

    app.py genera una muestra de soluciones

--- back/app.py
+++ back/app.py
@@ -1,17 +1,29 @@
-from time import time
 from src.domain.coder import Coder
 from src.domain.recruiter import Recruiter
-from src.main import create_list_of_matches, create_list_of_combinations
+from src.main import (
+    create_list_of_matches,
+    create_list_of_combinations,
+    filter_invalid_combinations,
+    filter_repeated_meetings,
+)
+import time
 
-ainara = Coder("ainara", ["Bilbao", "Barcelona"], ["python", "vue"])
+ainara = Coder("ainara")
+jeff = Coder("jeff")
+joseph = Coder("joseph")
+ainhoa = Coder("ainhoa")
+iker = Coder("iker")
 
-perla = Recruiter("perla", ["Bilbao"], ["python", "angular"], ["ingles"])
-laura = Recruiter("laura", ["Bilbao"], ["python", "angular"], ["ingles"])
+perla = Recruiter("perla")
+laura = Recruiter("laura")
+ana = Recruiter("ana")
 
-coder_list = [ainara]
+
+coder_list = [ainara, jeff]
 recruiter_list = [perla, laura]
 
 number_of_meetings = 2
+
 match_list = create_list_of_matches(coder_list, recruiter_list, number_of_meetings)
 
 match_list_printable = []
@@ -21,11 +33,23 @@ for match in match_list:
 
 print(match_list_printable)
 
+
 time_slots = len(recruiter_list * number_of_meetings)
+
+start_time = time.time()
+
 solutions_list = create_list_of_combinations(match_list, time_slots)
 
+pre_filtered_solutions = filter_invalid_combinations(solutions_list)
+
+filtered_solutions = filter_repeated_meetings(pre_filtered_solutions)
+
+print("--- %s seconds ---" % (time.time() - start_time))
+
+# print(sum(1 for i in filtered_solutions))
+
 solution_number = 0
-for solution in solutions_list:
+for solution in filtered_solutions:
     solution_str = ""
     solution_number += 1
     for match in solution:
